a41d4ad795be31e43b0ce62669708745ba976b9d
Viktor <fvi7707@gmail.com>
2022-06-27 02:19:59 +0300

    Viktor
    27.06.22

---
+++ eco.S.H.O.P/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eco.S.H.O.P.Areas.Admin.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
